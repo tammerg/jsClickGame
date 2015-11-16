ea165cf3652169006e77f2474ff727eb27fbf56a
Tammer Galal <tg385@scarletmail.rutgers.edu>
2015-11-15 19:41:45 -0500

    finished clickGame.js. Game is done.

---
+++ js/clickGame.js
@@ -0,0 +1,14 @@
+var counter = 0;
+function countup() {
+  counter = counter + 1;
+  }
+
+
+function countdown() {
+  setTimeout(function countandgive()
+    { 
+    alert("TIME-OUT! You got " +counter+ " Teams!");
+     
+     }, 20000);
+
+}
